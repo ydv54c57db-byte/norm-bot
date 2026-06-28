a69b1f194301809223037866a07b32ab49b579f2
ydv54c57db-byte <ydv54c57db@privaterelay.appleid.com>
2026-06-28 23:22:17 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ TOKEN = os.getenv("TOKEN")
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.text.lower() == "норм":
-        await update.message.reply_text("норм")
+        await update.message.reply_text("Норм")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
