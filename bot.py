fead0cfd99039d08cb9cf7599298eef714238b40
ydv54c57db-byte <ydv54c57db@privaterelay.appleid.com>
2026-06-29 00:14:38 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -3,31 +3,22 @@ from telegram.ext import Application, MessageHandler, ContextTypes, filters
 import os
 
 TOKEN = os.getenv("TOKEN")
+
 CHAT_ID = -5458919378
 
 
-# 💬 реакція на "норм"
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text.lower()
+
+    # тест що бот взагалі працює
+    print("MSG:", text)
 
     if "норм" in text:
         await update.message.reply_text("Норм")
 
 
-# 🌅 ранкове повідомлення (через простий job)
-async def morning_message(context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=CHAT_ID,
-        text="Най ваш день буде норм"
-    )
-
-
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
-
-# ⏰ правильно додаємо job queue
-job_queue = app.job_queue
-job_queue.run_daily(morning_message, time=7, 0)
 
 app.run_polling()
