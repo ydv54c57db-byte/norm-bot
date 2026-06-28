aa6ec889ef09db888cef536c340806043046761b
ydv54c57db-byte <ydv54c57db@privaterelay.appleid.com>
2026-06-28 22:16:24 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.text.lower() == "норм":
+        await update.message.reply_text("норм")
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+
+app.run_polling()
