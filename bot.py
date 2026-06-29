25b8f28b6402b8136983ff5370f4ca657f13ebbe
ydv54c57db-byte <ydv54c57db@privaterelay.appleid.com>
2026-06-29 09:24:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes, filters
 import os
 from datetime import time
+from zoneinfo import ZoneInfo
 
 TOKEN = os.getenv("TOKEN")
 
@@ -16,7 +17,7 @@ async def morning_message(context: ContextTypes.DEFAULT_TYPE):
     chat_id = -5458919378
     await context.bot.send_message(
         chat_id=chat_id,
-        text="Най ваш день буде норм"
+        text="Хааай норми\nНай ваш день буде норм"
     )
 
 app = Application.builder().token(TOKEN).build()
@@ -25,6 +26,9 @@ app = Application.builder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
 # планування
-
+app.job_queue.run_daily(
+    morning_message,
+    time=time(hour=7, minute=0, tzinfo=ZoneInfo("Europe/Kyiv"))
+)
 
 app.run_polling()
